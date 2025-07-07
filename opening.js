c1d952d681cdd495eeedcc97cfd4bf180e4e041f
anti-social-coder <nathanielmcqueen2021@gmail.com>
2025-07-07 14:20:44 -0400

    Add files via upload

---
+++ opening.js
@@ -0,0 +1,8 @@
+window.alert("----WARNING----");
+window.alert("Your accessing a highly Classified site");
+window.alert("Proceed with caution");
+window.alert("You will be logged");
+
+//this is an alert
+
+
